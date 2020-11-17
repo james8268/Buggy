21de5178b930cbd366518912771e96131b2c7ed4
james8268 <72760747+james8268@users.noreply.github.com>
2020-11-17 11:55:13 +0000

    Update ESP32_WiFi.ino

--- ESP32_WiFi/ESP32_WiFi.ino
+++ ESP32_WiFi/ESP32_WiFi.ino
@@ -1,3 +1,6 @@
+// This sketch is to be uploaded to the ESP32 when a WiFi connection is wanted rather than a Bluetooth connection. 
+
+
 #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
@@ -6,47 +9,44 @@
 
 
 
-#define transmit 18
-#define lost 19
+#define transmit 18   // pin 18 sends a signal to the Arduino 
+#define lost 19       // pin 19 sends a signal to the arduino if the WiFi connection is lost, this allows the buggy to stop moving. 
 
 String content = "";
 char character;
 
-char auth[]= "jVAGDLSq2_VTo2K_l74wrM509taT5Bty";
+char auth[]= "jVAGDLSq2_VTo2K_l74wrM509taT5Bty";  // author token allows the Blynk app to to link with this project.
 
-char ssid[]="VM9234142";
+char ssid[]="VM9234142";    //internet SSID and passwork. 
 char pass[]= "hgyb9tqPdhfw";
 
-//char ssid[]="Ravenhill";
-//char pass[]= "r8v3n4i1l";
 
-
-WidgetTerminal terminal(V1);
+WidgetTerminal terminal(V1);   // declares the terminal is linked to the virtual pin 1.
 
   
 
-BLYNK_WRITE(V1) {  
+BLYNK_WRITE(V1) {       // this function allows the terminal on the mobile device to communicate to the ESP32 Serial. 
 
   // Print all parameter values
-  for (auto i = param.begin(); i < param.end(); ++i) {
+  for (auto i = param.begin(); i < param.end(); ++i) { // this for loop sets the condtions needed to read the messages as a string from the terminal  
     Serial.println(i.asString());
    
   }
-      digitalWrite(transmit, HIGH);
-
-   delay(500);
+  digitalWrite(transmit, HIGH);   // when a string of communication recieved by the esp32 it will cause the transmit pin to output as high for half a second.
+  delay(500);                    // This is then used by the arduino mega in the operation.roam() 
   digitalWrite(transmit, LOW);
+
 
 }
 
 
 void SerialInput() {
-  if (Serial.available()) {  // Watch for Serial data
-    character = Serial.read();  // Read Serial character
-    content.concat(character);  // Add character to holding String
-    if (character == '\n'){  // If last character is new line...
-      Blynk.virtualWrite(V1, content);  // ...send string to terminal
-      content = "";  // Clear holding String
+  if (Serial.available()) {  
+    character = Serial.read();  // when a serial is available, read what has been printed
+    content.concat(character); 
+    if (character == '\n'){  
+      Blynk.virtualWrite(V1, content);  // the characters have been gathered and are then sent to the mobile devices terminal 
+      content = "";  // Clear the String to be able to reciver a new message without repeating what was said previously. 
     }
   }
 }
@@ -55,10 +55,10 @@ void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
 
-Blynk.begin(auth, ssid, pass);
-terminal.clear();
-Blynk.virtualWrite(V1, "Welcome MR Ravenhill, I'm ready to recieve commands");
-pinMode(transmit,OUTPUT);
+Blynk.begin(auth, ssid, pass);     // this is where the esp32 connects to the WiFi and Blynk Cloud. 
+terminal.clear();                  // clear the mobile terminal
+Blynk.virtualWrite(V1, "Welcome MR Ravenhill, I'm ready to recieve commands");     // welcome message printed in the mobile terminal.
+pinMode(transmit,OUTPUT);   //pin set ups for the transmit and lost pin. 
 pinMode(lost, OUTPUT);
 
 }
@@ -67,12 +67,12 @@ pinMode(lost, OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
- Blynk.run();
+ Blynk.run();    //this runs all the Blynk functions 
 SerialInput();
 
-if(Blynk.connect()==false){
-  digitalWrite(lost, HIGH);
- Serial.println("Connection lost MEE");
+if(Blynk.connected()==false){  // if connection is lost then this if loop is entered.  Blynk.connect() can be used to send a ping to the router. 
+  digitalWrite(lost, HIGH);    // it communicates to the arduino mega through a digital pin 
+ Serial.println("Connection lost MEE");    // this is printed in the serial, if wifi connection is lost there will be no way to communicate to the mobile device so there is no need for this communication method. 
   delay(5000);
   digitalWrite(lost, LOW);}
 
