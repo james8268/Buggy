36f897154fe3e0da31751121f4f036ed7bdcb5ff
james8268 <72760747+james8268@users.noreply.github.com>
2020-11-17 12:00:05 +0000

    Update master.ino

--- Master/Master/master.ino
+++ Master/Master/master.ino
@@ -1,4 +1,6 @@
-// This is the main code.          // taken baud rates:9600(bluetooth), 4800 (Ultrasound)
+// This is the main code, It is uploaded to the Arduino Mega. It contains the setup functions for specific libraries. 
+
+// taken baud rates:9600(bluetooth), 4800 (Ultrasound)
 
 #include "Arduino.h"
 #include"Motor.h"
@@ -9,7 +11,7 @@
 
 void setup() {
 
-Motor.SETUP();
+Motor.SETUP();     
 Ultra.SETUP();
 Bluetooth.SETUP();
 operation.SETUP();
@@ -18,13 +20,11 @@ operation.SETUP();
 
 void loop() {
 
-Bluetooth.read_message();  
+Bluetooth.read_message();   // this reads messages sent over WiFi or Bluetooth to the esp32 and actions them. 
 
-operation.level();
-operation.noComs();
+operation.level();  // this function will stop the buggy if the tilt sensor is triggered, a buzzer and warning message will alert the user. 
+operation.noComs();  // this function monitors the connection between the ESP32 and wifi/bluetooth connection. If connection is lost the front red LED will light up. 
 
 }
 
 
-
- // use an interrupt for overide control
