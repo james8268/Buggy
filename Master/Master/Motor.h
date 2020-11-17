ebc06fdd8dcb6f646c0c23115d266d9023e1732a
james8268 <72760747+james8268@users.noreply.github.com>
2020-11-17 12:04:46 +0000

    Update Motor.h

--- Master/Master/Motor.h
+++ Master/Master/Motor.h
@@ -10,7 +10,7 @@ Motorclass();
     void forwards2();   // multiple forwards functions which correspond to the speed of the motors.
     void forwards3();
     
-    void backwards();
+    void backwards();  // functions to reverse, rotate left, rotate right and halt the buggy
     void left90();
     void right90();
     void halt();
